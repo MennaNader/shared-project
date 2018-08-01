bca945dbd042290e0fb45bd5785c33a6f6a76c6b
waleed.tayseer <waleed.tayseer@itworx.com>
2018-08-01 16:49:37 +0200

    added font awesome, webpack fixes

--- src/routers/mainRouter.js
+++ src/routers/mainRouter.js
@@ -1,13 +1,14 @@
 import React from 'react';
-import { BrowserRouter, Route } from "react-router-dom";
+import { BrowserRouter, Route } from 'react-router-dom';
 import Locker from 'components/Locker';
+import '@fortawesome/fontawesome-free/css/all.css';
 
-const MainRouter = () => {
-	return (<BrowserRouter>
-		<div>
-			<Route path="/" component={Locker} />
-		</div>
-	</BrowserRouter>)
-}
+const MainRouter = () => (
+    <BrowserRouter>
+        <div>
+            <Route path="/" component={Locker} />
+        </div>
+    </BrowserRouter>
+);
 
 export default MainRouter;
